792f733ef16872f63cf32cb1a40b5c9ec408fe3e
Sergio Sánchez <sergio_sanchez@ciencias.unam.mx>
2019-09-18 10:36:41 -0500

    Notebooks para tratamiento de datos

---
+++ dowload_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASE[1]=cuadrantes
+TIPO[2]=geojson
+wget -O ${BASE}.${TIPO} "https://datos.cdmx.gob.mx/explore/dataset/cuadrantes/download/?format=${TIPO}&timezone=America/Mexico_City"
+
+BASE[2]=comisiones-vecinales
+TIPO[1]=json
+wget -O ${BASE}.${TIPO} "https://datos.cdmx.gob.mx/explore/dataset/cuadrantes/download/?format=${TIPO}&timezone=America/Mexico_City"
+
+
+BASE=carpetas-de-investigacion-pgj-de-la-ciudad-de-mexico
+TIPO=json
+ANIO=2019
+MES=Febrero
+wget -O ${BASE}_${ANIO}_${MES}.${TIPO} "https://datos.cdmx.gob.mx/explore/dataset/${BASE}/download/?format=${TIPO}&disjunctive.delito=true&refine.ao_hechos=${ANIO}&refine.mes_hechos=${MES}&timezone=America/Mexico_City"
+
